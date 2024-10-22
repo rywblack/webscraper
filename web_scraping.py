08993975dbc586d44127cb15f90a6db5d6728db3
rywblack <ry.black@me.com>
2024-10-21 21:25:35 -0500

    B: added finishing touches, now fully user friendly

--- web_scraping.py
+++ web_scraping.py
@@ -1,6 +1,7 @@
 from bs4 import BeautifulSoup
 import requests
 import re
+import os
 
 def printPage(inp,url,genres):
     page = requests.get(url).text
@@ -16,9 +17,12 @@ def printPage(inp,url,genres):
         print("")
         print(doc.find(class_="current").get_text(strip=True))
         print("")
-        inp2 = input("What page number would you like to go to? ")
+        inp2 = input("What page number would you like to go to? (type 'return' to return to the genre selection screen) ")
         finalPageNum = int(doc.find(class_="current").get_text(strip=True)[len(doc.find(class_="current").get_text(strip=True))-1])
         def checkPageNum(inp,inp2,finalPageNum,genres):
+            if inp2.lower() == 'return':
+                os.system('clear')
+                homeScreen()
             try:
                 inp2 = int(inp2)
                 finalPageNum = int(finalPageNum)
@@ -28,39 +32,61 @@ def printPage(inp,url,genres):
                 inp2 = input("invalid page number, please try again: ")
                 checkPageNum(inp,inp2,finalPageNum,genres)
             else:
+                os.system('clear')
                 printPage(inp,f"https://books.toscrape.com/catalogue/category/books/{inp.replace(' ', '-')}_{genres.index(inp)+2}/page-{inp2}.html",genres)
         
         checkPageNum(inp,inp2,finalPageNum,genres)
+    else:
+        print("")
+        inp2 = input("Type 'return' to return to the genre selection screen: ")
+        inp2 = inp2.lower()
+        def checkReturn(inp2):
+            if inp2 != "return":
+                inp2 = input("invalid input, please try again: ")
+                checkReturn(inp2)
+            else:
+                os.system('clear')
+                homeScreen()
+        checkReturn(inp2)
 
 
+def homeScreen():
 
-print("WELCOME, PLEASE PICK A GENRE FROM BELOW:")
-print("")
+    print("WELCOME, PLEASE PICK A GENRE FROM BELOW:")
+    print("")
 
-url = f"https://books.toscrape.com/index.html"
-page = requests.get(url).text
-doc = BeautifulSoup(page, "html.parser")
+    url = f"https://books.toscrape.com/index.html"
+    page = requests.get(url).text
+    doc = BeautifulSoup(page, "html.parser")
 
-parent_ul = doc.find(class_="nav nav-list")
-grandchild_ul = parent_ul.find("ul")
+    parent_ul = doc.find(class_="nav nav-list")
+    grandchild_ul = parent_ul.find("ul")
 
-genres = []
-for li in grandchild_ul.find_all("li"):
-    genres.append(li.get_text(strip=True))
+    genres = []
+    for li in grandchild_ul.find_all("li"):
+        genres.append(li.get_text(strip=True))
 
-print(", ".join(genres))
+    print(", ".join(genres))
 
-print("")
+    print("")
 
-inp = input()
+    inp = input()
 
-genres = list(map(lambda x: x.lower(),genres))
-inp = inp.lower()
+    genres = list(map(lambda x: x.lower(),genres))
+    inp = inp.lower()
+    def checkGenre(inp):
+        if inp in genres:
+            url = f"https://books.toscrape.com/catalogue/category/books/{inp.replace(' ', '-')}_{genres.index(inp)+2}/index.html"
+            os.system('clear')
+            printPage(inp,url,genres)
+        else:
+            inp = input("genre not found, please try again: ")
+            checkGenre(inp)
+    checkGenre(inp)
 
-if inp in genres:
-    url = f"https://books.toscrape.com/catalogue/category/books/{inp.replace(' ', '-')}_{genres.index(inp)+2}/index.html"
-    printPage(inp,url,genres)
-else:
-    print("genre not found")
 
+def main():
+    homeScreen()
 
+if __name__ == "__main__":
+    main()
